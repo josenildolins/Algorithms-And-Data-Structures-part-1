7477b3652414391ed6afe3498a63f5fcd88af037
Josenildolins <josenildo_004@hotmail.com>
2020-02-26 09:38:11 -0300

    It was add Stack class, Calculator class;

---
+++ DataStructures/PostFixNotation/Calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PostFixNotation
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            args = new string[] { "5", "6", "7", "*","+", "1", "-" };
+
+            //The stack of integers not yet operated on
+            Stack<int> values = new Stack<int>();
+
+            foreach(string token in args)
+            {
+                // if the value is an integer...
+                int value;
+                if (int.TryParse(token, out value))
+                {
+                    //...push it to the stack
+                    values.Push(value);
+                }
+                else
+                {
+                    // otherwise evaluate the expression
+                    int righthandSide = values.Pop();
+                    int lefthandSide = values.Pop();
+
+                    // ... and pop the result back to the stack
+                    switch (token)
+                    {
+                        case "+":
+                            values.Push(lefthandSide + righthandSide);
+                            break;
+                        case "-":
+                            values.Push(lefthandSide - righthandSide);
+                            break;
+                        case "*":
+                            values.Push(lefthandSide * righthandSide);
+                            break;
+                        case "/":
+                            values.Push(lefthandSide/ righthandSide);
+                            break;
+                        case "%":
+                            values.Push(lefthandSide % righthandSide);
+                            break;
+                        default:
+                            throw new ArgumentException(string.Format("Unrecognized token: {0}", token));
+                    }
+                }
+            }
+            // the last item on the stack is the result
+            Console.WriteLine(values.Pop());
+        }
+    }
+}
---
+++ DataStructures/Stack/Stack.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack.Array
+{
+    /// <summary>
+    /// A last In First Out (LIFO) Collection Implemented as an array.
+    /// </summary>
+    /// <typeparam name="T"> The type of item contained in the stack</typeparam>
+    public class Stack<T> : IEnumerable<T>
+    {
+
+        /// <summary>
+        /// Enumerates each items in the stack in LIFO order. the stack remain unaltered.
+        /// </summary>
+        /// <returns>The LIFO enumerator</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _size; i >= 0; i--)
+            {
+                yield return _items[i];
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+
+            return GetEnumerator();
+
+        }
+
+        // The array of items contained in the stack. Initiated to 0 length,
+        // will grow as needed during push
+        T[] _items = new T[0];
+
+        // The current number of items in the stack
+        int _size;
+
+        /// <summary>
+        /// Adds the specifidenitems to the stack
+        /// </summary>
+        /// <param name="items"></param>
+        public void Push(T items)
+        {
+            //_size  = 0 ... first push
+            // _size == length .... growth boundary
+            if (_size == _items.Length)
+            {
+                // initial size of 4, otherwise double the current length
+                int newLength = _size == 0 ? 4 : _size * 2;
+
+                // allocate, copy and assign the new array
+                T[] newArray = new T[newLength];
+                _items.CopyTo(newArray, 0);
+                _items = newArray;
+            }
+
+            // add the item to the stack array and increase the size
+            _items[_size] = items;
+            _size++;
+        }
+
+        /// <summary>
+        /// Removes and returns the top item from the stack
+        /// </summary>
+        /// <returns>The top-most items in the stack</returns>
+        public T Pop()
+        {
+
+            if (_size == 0)
+            {
+                throw new InvalidOperationException("the stack is empty");
+            }
+            _size--;
+
+            return _items[_size];
+        }
+
+        public T peek()
+        {
+            if (_size == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+
+            }
+
+            return _items[_size - 1];
+
+        }
+
+        /// <summary>
+        /// The current number of items in the stack
+        /// </summary>
+        public int Count
+        {
+
+            get
+            {
+                return _size;
+            }
+        }
+
+        /// <summary>
+        /// Remove all items from the stack
+        /// </summary>
+        public void clear()
+        {
+            _size = 0;
+        }
+
+
+
+
+    }
+}
+
